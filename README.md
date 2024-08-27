833ddd7221790ad80e4b3df31c68be372b3ea9ac
Yoskoslowich Fernandes <yoskos@outlook.com>
2024-08-27 20:13:41 -0300

    feat: integrates database

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
+## Fast Zero
+
 #### Activate virtual env
 
 ```
 source "$( poetry env info --path )/bin/activate"
+```
+
+#### Run
+
+```
+task run
+```
+
+#### Create migration
+
+```
+alembic revision --autogenerate -m "<message>"
+```
+
+#### Apply migrations
+
+```
+alembic upgrade head
 ```
---
+++ tests/test_db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session):
+    user = User(username="test", password="test", email="test@mail.com")
+    session.add(user)
+    session.commit()
+    query = select(User).where(User.email == "test@mail.com")
+    result = session.scalar(query)
+    session.refresh(user)
+    assert result.username == "test"
+    assert user.id == 1
